0e55d64f3a8368ccc891f044717ad3706b9439e0
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2023-09-30 16:13:25 +0100

    v9

--- src/main/java/com/example/demo/entite/Participant.java
+++ src/main/java/com/example/demo/entite/Participant.java
@@ -5,10 +5,14 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "participant")
 public class Participant extends Utilisateur{
-
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateDeParticipation = new Date();
 }
